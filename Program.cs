96033a9684fcc682afe43d4cd9b245791e0191f3
parsa <parsa32569@gmail.com>
2024-01-11 18:37:49 +0330

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+Dictionary<string, string> countries = new()
+{
+    {"iran", "fa-IR"},
+};
+
+KeyValuePair<string, string> country = new();
+if (args.Length == 1)
+{
+    country = countries.First();
+}
+else
+{
+    Console.WriteLine("Country Not Found");
+    return;
+}
+
+CultureInfo givenCulture = CultureInfo.GetCultureInfo(country.Value);
+Thread.CurrentThread.CurrentCulture = givenCulture;
+Console.WriteLine($"Clock '{givenCulture.DisplayName}' : ");
+Console.WriteLine("------------------------------");
+Console.WriteLine(DateTime.Now);
+Console.WriteLine("------------------------------");
+foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+{
+    if (item.ToString() == DateTime.Now.DayOfWeek.ToString())
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("> ");
+    }
+    Console.WriteLine($"{item}");
+    Console.ResetColor();
+}
